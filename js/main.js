52e78ac384794a059584d259e343f7eb3f81ba4c
bumblebroo <otis.gustafsson@hotmail.com>
2024-02-05 18:04:11 +0100

    initial

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+    rutor = document.getElementById("Rutor");
+
+    tal = [];    
+    for(Ruta of rutor.getElementsByTagName("div")){
+        p = Ruta.getElementsByTagName("p");
+        console.log(p)
+        while(true){
+            randomTal = Math.round(Math.random() * 100);
+            if(!tal.includes(randomTal)){
+                tal.push(randomTal);
+                p.innerText = randomTal;
+                console.log(tal);
+                break;
+            }
+        }
+    }
+
+    console.log(rutor);
+    rutor.addEventListener("click", function (event){
+        if(event.target.tagName=="DIV"){
+            event.target.children[0].classList.toggle("visible");
+            event.target.children[1].classList.toggle("visible");
+
+        }
+    });
+}
