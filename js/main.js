1d2bc25ef23eba466123090247e24f6e41521ee4
bumblebroo <otis.gustafsson@hotmail.com>
2024-02-06 12:15:43 +0100

    less complicated/not toggle

--- js/main.js
+++ js/main.js
@@ -2,26 +2,19 @@ window.onload = function(){
     rutor = document.getElementById("Rutor");
 
     tal = [];    
-    for(Ruta of rutor.getElementsByTagName("div")){
-        p = Ruta.getElementsByTagName("p")[1];
-        console.log(p)
-        while(true){
-            randomTal = Math.round(Math.random() * 100);
-            if(!tal.includes(randomTal)){
-                tal.push(randomTal);
-                p.innerText = randomTal;
-                console.log(tal);
-                break;
-            }
-        }
-    }
-
     console.log(rutor);
-    rutor.addEventListener("click", function (event){
-        if(event.target.tagName=="DIV"){
-            event.target.children[0].classList.toggle("visible");
-            event.target.children[1].classList.toggle("visible");
-
+    rutor.addEventListener("mouseover", function (event){
+        if(event.target.tagName=="DIV" && event.target.id != "Rutor" && event.target.children[0].innerText == "?"){
+            randomTal = 0;
+            while(true){
+                randomTal = Math.round(Math.random() * 100);
+                if(!tal.includes(randomTal)){
+                    tal.push(randomTal);
+                    console.log(tal);
+                    break;
+                }
+            }
+            event.target.children[0].innerText = randomTal;
         }
     });
 }
