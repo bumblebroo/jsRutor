998ab92f7b25c349534008558c945a84636e2058
bumblebroo <otis.gustafsson@hotmail.com>
2024-02-08 13:43:56 +0100

    rework

--- js/main.js
+++ js/main.js
@@ -1,22 +1,59 @@
 var tal = [];
-var ruta1;
-var ruta2;
+var clickedElement;
+var rutor;
 window.onload = function(){
     rutor = document.getElementById("Rutor");
    
     console.log(rutor);
-    rutor.addEventListener("mouseover", function (event){
-        if(event.target.tagName=="DIV" && event.target.id != "Rutor" && event.target.children[0].innerText == "?"){
+    rutor.addEventListener("mouseover", hoverHandler);
+    rutor.addEventListener("click", clickHandler);
+    function hoverHandler(event){
+        if(event.target.tagName=="DIV" && event.target.id != "Rutor" && event.target.innerText == "?"){
             randomTal = 0;
             while(true){
-                randomTal = Math.round(Math.random() * 100);
+                randomTal = Math.floor(Math.random() * 100 + 1);
                 if(!tal.includes(randomTal)){
                     tal.push(randomTal);
                     console.log(tal);
                     break;
                 }
             }
-            event.target.children[0].innerText = randomTal;
+            event.target.innerText = randomTal;
         }
-    });
+    }
+    function clickHandler(event){
+        if(event.target.tagName=="DIV" && event.target.id != "Rutor" && event.target.innerText != "?"){
+            if(clickedElement == null){
+                clickedElement = event.target;
+            }
+            else{
+                let _tempRuta = clickedElement.innerText;
+                clickedElement.innerText = event.target.innerText;
+                event.target.innerText = _tempRuta;
+    
+                clickedElement = null;
+                tempRuta = null;
+                
+                let _win = checkOrder();
+                console.log("u alive?")
+                if(_win){
+                    console.log("WIN");
+                }
+            }
+        }
+    }
+    function checkOrder(){
+        let _counter = 0;
+        console.log(rutor);
+        for(ruta in rutor){
+            if(parseInt(ruta.innerText) >= _counter){
+                console.log(counter);
+                _counter=parseInt(ruta.innerText);
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
 }
